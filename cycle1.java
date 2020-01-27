fd9fb7ae41e2c702521f64e076aa3f6372386da8
ABONK87 <ant.bonk@mail.ru>
2020-01-27 11:24:57 +0300

    Add files via upload

---
+++ cycle1.java
@@ -0,0 +1,17 @@
+package Basics_of_software_code_development;
+
+public class cycle1 {
+    public static void main(String[] args) {
+        int corner1 = 45;
+        int corner2 = 45;
+        int corner3 = 180 - corner1 - corner2;
+        if (corner3 > 0) {
+            if (corner1 == 90 || corner2 == 90 || corner3 == 90) {
+                System.out.println("Треугольник существует и он прямоугольный");
+            }
+            else System.out.println("Треугольник существует");
+        }
+        else System.out.println("Треугольник не существует");
+
+    }
+}
