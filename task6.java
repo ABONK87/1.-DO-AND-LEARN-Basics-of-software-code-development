a69568afccf86e2aad718375a8bf1eb69862fc1b
ABONK87 <ant.bonk@mail.ru>
2020-01-27 10:59:11 +0300

    Add files via upload

---
+++ task6.java
@@ -0,0 +1,24 @@
+package Basics_of_software_code_development;
+
+public class task6 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 1;
+        if (y > 0) {
+            if (x > -2 && x < 2) {
+                System.out.println("true");
+            }
+            else {
+                System.out.println("false");
+            }
+        }
+        else {
+            if (x > -4 && x < 4) {
+                System.out.println("true");
+            }
+            else {
+                System.out.println("false");
+            }
+        }
+    }
+}
